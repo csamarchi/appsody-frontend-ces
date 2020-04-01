87a3d3bfe192da673647bdd6d73f1cbf75ff1d59
Christine Samarchi <christine.samarchi@Christines-MBP.lan>
2020-04-01 15:02:06 -0500

    test3

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use(function(err, req, res, next) {
   console.log(err)
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('err');
 });
 
 module.exports.app = app;
